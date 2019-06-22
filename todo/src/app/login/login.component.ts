4f15f62ed1660a296eae5d83aeb887c00c955de6
Nishant.Nishant <nishant.nishant@cognizant.com>
2019-06-22 21:23:37 +0530

    adding seperate authentication service

--- todo/src/app/login/login.component.ts
+++ todo/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HardcodedAuthenticationService } from '../service/hardcoded-authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -13,14 +14,18 @@ export class LoginComponent implements OnInit {
   password = '';
   errorMessage = 'Invalid credentials';
   invalidLogin = false;
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+    private hardcodedAuthenticationService: HardcodedAuthenticationService) { }
 
   ngOnInit() {
   }
 
   handleLogin() {
 
-    if (this.username === 'nishant' && this.password === 'dummy') {
+    // if (this.username === 'nishant' && this.password === 'dummy') {
+
+    if (this.hardcodedAuthenticationService.authenticate(this
+      .username, this.password)) {
       this.invalidLogin = false;
       this.router.navigate(['welcome', this.username]);
     } else {
