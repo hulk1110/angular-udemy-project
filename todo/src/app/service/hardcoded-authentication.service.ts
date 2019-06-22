fb8190b9c4edbfcd5b1b22dfc47ba501bcef7e56
Nishant.Nishant <nishant.nishant@cognizant.com>
2019-06-22 21:32:39 +0530

    adding session storage

--- todo/src/app/service/hardcoded-authentication.service.ts
+++ todo/src/app/service/hardcoded-authentication.service.ts
@@ -8,10 +8,19 @@ export class HardcodedAuthenticationService {
   constructor() { }
 
   authenticate(username, password) {
+    console.log('before' + this.isUserLoggedIN());
     if (username === 'nishant' && password === 'dummy') {
+      sessionStorage.setItem('authenticateruser', username);
+      console.log('before' + this.isUserLoggedIN());
       return true;
     }
     return false;
 
+  }
+
+  isUserLoggedIN() {
+
+    const user = sessionStorage.getItem('authenticateruser');
+    return !(user === null);
   }
 }
