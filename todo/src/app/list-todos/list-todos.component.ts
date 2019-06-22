d1e2cca5be961387499954bfb9ee15a27ea6c038
Nishant.Nishant <nishant.nishant@cognizant.com>
2019-06-22 19:28:26 +0530

    adding todo class

--- todo/src/app/list-todos/list-todos.component.ts
+++ todo/src/app/list-todos/list-todos.component.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Todo {
+
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ) {
+
+  }
+
+}
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
@@ -7,12 +20,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListTodosComponent implements OnInit {
 
-todos = [
+  todos = [
+    new Todo(1, 'learn to code', false, new Date()),
+    new Todo(2, 'book the ticket', true, new Date()),
+    new Todo(3, 'pay bills', true, new Date()),
 
-  {id: 1, description: 'learn to code'},
-  {id: 2, description: 'book the ticket'},
-  {id: 3, description: 'visit home'}
-];
+  ];
 
 
   constructor() { }
