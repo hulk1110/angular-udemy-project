eb4794b1d5e89d8d0c849afdfd1fe7856504f212
Nishant.Nishant <nishant.nishant@cognizant.com>
2019-06-22 11:21:57 +0530

    adding rotuing and error component

--- todo/src/app/app-routing.module.ts
+++ todo/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from 'src/app/login/login.component';
+import { WelcomeComponent } from 'src/app/welcome/welcome.component';
+import { ErrorComponent } from 'src/app/error/error.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'welcome', component: WelcomeComponent},
+  { path: '**', component: ErrorComponent }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
